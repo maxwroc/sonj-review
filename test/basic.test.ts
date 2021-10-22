1824e700e0f788fe0922ac36536bf031028c50e2
Max Chodorowski <max.chodorowski@gmail.com>
2021-10-22 22:22:01 +0100

    Added missing test

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -11,8 +11,7 @@ test("Root element rendered", async () => {
 test("Root element expanded", async () => {
     const elem = await initPageAndReturnViewerElem({ number: 1, string: "test string", float: 3.456, bool: true, emptyArray: [], emptyObject: {} });
 
-    const root = await page.$("#root");
-    await root!.click();
+    await page.click("#root");
     
     expect(await elem!.screenshot()).toMatchImageSnapshot();
 });
@@ -20,14 +19,25 @@ test("Root element expanded", async () => {
 test("Empty arrays and objects not expandable", async () => {
     const elem = await initPageAndReturnViewerElem({ arrayNode: [], objectNode: {} });
 
-    const root = await page.$("#root");
-    await root!.click();
+    await page.click("#root");
 
     const arrayNodeElem = await page.$("#root-arrayNode");
     const objectNodeElem = await page.$("#root-objectNode");
 
     expect(await arrayNodeElem!.$(".prop-expand") !== null).toBeFalsy();
     expect(await objectNodeElem!.$(".prop-expand") !== null).toBeFalsy();
+});
+
+test("Root element clicked twice", async () => {
+    const elem = await initPageAndReturnViewerElem({ number: 1 });
+
+    // check if element is not visible
+    expect(await page.$("#root-number")).toBeFalsy();
+
+    await page.click("#root");
+
+    // check if element is visible
+    expect(await page.$("#root-number")).toBeTruthy();
 });
 
 const initPageAndReturnViewerElem = async (data: any) => {
